8de8e78ba3c22daae92cc6734f3792945ba6bf8f
cindeem <cindee@berkeley.edu>
2013-05-16 20:44:34 -0700

    Add data cleaning and testing structure

--- fslnets/simple_fslnets.py
+++ fslnets/simple_fslnets.py
@@ -24,4 +24,38 @@ reshape 1 X ncomponents x ncomponents
  DD are wanted components, will regress out aanything not in this list
 """
 
+import os
+from glob import glob
+
+def normalise_data(dat):
+    """ demans and divides by std"""
+    # demean each column
+    tmp = (dat - dat.mean(0)) 
+    tmp = tmp / tmp.std()
+    return tmp
+
+def parse_idx(good, n):
+    """ given a list of good indicies (good), out of
+    n indicies, return an array of
+    good_idx
+    bad_idx"""
+    all_idx = np.arange(n)
+    bad_idx = np.array([x for x in all_idx if not x in good])
+    good_idx = np.array([ x for x in all_idx if x in good])
+    test_idx = sorted(np.concatenate((good_idx, bad_idx)))
+    if not all([x in good_idx for x in good]):
+        raise IOError('index issue %s not in range %s'%(good, all_idx))
+    return good_idx, bad_idx
+
+
+def remove_regress_bad_components(dat, good_idx):
+    good_idx = np.array(good_idx)
+
+    good = dat[:,good_idx]
+
+
+
+
+
+
 
